ac5f3fe659a55b71da8e3436780828138cda422b
David Ammeraal <david.ammeraal@gmail.com>
2014-09-18 10:32:26 +0200

    Selected country saved in URL parameter
    -You can now save the selected country to the URL using the history
    API. When a user selects a country, a parameter is added to the URL so
    that when the user opens another page and he wants to go back to the
    map page, the last selected country will be selected.
    
    -Updated with latest styles by Khris

--- WebContent/eddie/apps/euscreenxlmap/components/template/template.js
+++ WebContent/eddie/apps/euscreenxlmap/components/template/template.js
@@ -2,111 +2,116 @@ var Template = function () {
     Component.apply(this, arguments);
     var self = this;
     
-    setTimeout(function(){
-    	self.initMap();
-    }, 250);
+    var mapData = {};
+    var mapSize = {};
     
+    self.initMap = function(region){
+    	console.log("initMap(" + region + ")");
+    	var args = {
+			map: 'europe_en',
+            enableZoom: true,
+            showTooltip: true,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            color: '#dedede',
+            onRegionClick: function(element, code, region)
+            {
+                // on region click
+            	// get region data
+                if(mapData[code] != undefined) {
+
+                    // show result section
+                    if($('.results-section').css('display') != "block") {
+                        $('.results-section').css({'display': 'block'});
+                    }
+                    
+                    // variables
+                    var countryData = mapData[code],
+                        countryProvider = countryData['providers'],
+                        mediaAmount = {'videos': 0, 'audios': 0, 'images': 0, 'texts': 0, 'series': 0},
+                        providerLink = "",
+                        providerList = "";
+                
+                    // count
+                    for (var item in countryProvider) {
+                        
+                        // set url
+                        //var url = "<li><a href='search-results.html?provider="+item+"'>SEARCH "+item.toUpperCase()+" CONTENT</a></li>";
+                        //providerLink += url;
+                        providerList += "<span class='provider-list'>"+countryProvider[item].name+ " ("+item.toUpperCase()+") <a href='/search.html?activeFields=%7B\"provider\"%3A%5B\""+escape(item)+"\"%5D%7D' class='box-link'>SEARCH "+item.toUpperCase()+" CONTENT</a></span>";
+                        
+                        // add the media
+                        mediaAmount['videos'] += countryProvider[item].videos;
+                        mediaAmount['audios'] += countryProvider[item].audios;
+                        mediaAmount['images'] += countryProvider[item].images;
+                        mediaAmount['texts'] += countryProvider[item].texts;
+                        mediaAmount['series'] += countryProvider[item].series;
+                    }
+
+                    // get general info like country name
+                    $('#selected-country').html(mapData[code].country);
+
+                    // set amount of medias
+                    $('#selected-videos').html(mediaAmount['videos']);
+                    $('#selected-audios').html(mediaAmount['audios']);
+                    $('#selected-images').html(mediaAmount['images']);
+                    $('#selected-texts').html(mediaAmount['texts']);
+                    $('#selected-series').html(mediaAmount['series']);
+
+                    // set providers info
+                    //$('#selected-searchlink').html(providerLink);
+                    $('#selected-providers').html(providerList);
+                } else {
+
+                    // show alert when no provider
+                    alert("No EUscreen provider from "+region);
+                }
+                
+                var objectToSend = {
+            		'code': code
+                }
+                eddie.putLou('history', 'setParameter(' + JSON.stringify(objectToSend) + ')');
+            }
+    	}
+    	
+    	 // get vmap size
+        mapSize.width = $('.maps').width();// + 30; // 30 is the padding
+        mapSize.height =  (mapSize.width * 3) / 4 + 15;
+        
+        // set map size
+        $('#vmap').css({
+            width: mapSize.width +'px',
+            height: mapSize.height +'px'
+        });
+
+        // load interactive map
+        // using jqvmap (MIT License)
+        $('#vmap').vectorMap(args);
+        
+    	// retrieve providers data (country, the amount of videos, audios etc.) from json file
+        // file has to be updated
+        $.getJSON( "/eddie/apps/euscreenxlelements/libs/jqvmap/data/euscreen.provider.data.json", function( data ) {
+            
+            // set variable
+            mapData = data;
+
+            // highlight countries in euscreen
+            // for easier search on the map
+            var colorsST = {},
+                highlightColor = "#c0c1c5";
+
+            // loop
+            for (var item in data) {
+                colorsST[item] = highlightColor;
+            }
+            
+            console.log(colorsST);
+            
+            // set
+            $('#vmap').vectorMap('set', 'colors', colorsST);
+            $('#jqvmap1_' + region).click();
+        });
+    }
 };
 
 Template.prototype = Object.create(Component.prototype);
-Template.prototype.initMap = function(){
-	var mapData = {},
-	mapSize = {};
-    
- // get vmap size
-    mapSize.width = $('.maps').width();// + 30; // 30 is the padding
-    mapSize.height =  (mapSize.width * 3) / 4 + 15;
-    
-    // set map size
-    $('#vmap').css({
-        width: mapSize.width +'px',
-        height: mapSize.height +'px'
-    });
-
-    // load interactive map
-    // using jqvmap (MIT License)
-    $('#vmap').vectorMap({
-        map: 'europe_en',
-        enableZoom: false,
-        showTooltip: true,
-        backgroundColor: '#fff',
-        borderColor: '#fff',
-        color: '#dedede',
-        onRegionClick: function(element, code, region)
-        {
-            // on region click
-        	// get region data
-            if(mapData[code] != undefined) {
-
-                // show result section
-                if($('.results-section').css('display') != "block") {
-                    $('.results-section').css({'display': 'block'});
-                }
-                
-                // variables
-                var countryData = mapData[code],
-                    countryProvider = countryData['providers'],
-                    mediaAmount = {'videos': 0, 'audios': 0, 'images': 0, 'texts': 0, 'series': 0},
-                    providerLink = "",
-                    providerList = "";
-            
-                // count
-                for (var item in countryProvider) {
-                    
-                    // set url
-                    //var url = "<li><a href='search-results.html?provider="+item+"'>SEARCH "+item.toUpperCase()+" CONTENT</a></li>";
-                    //providerLink += url;
-                    providerList += "<span class='provider-list'>"+countryProvider[item].name+ " ("+item.toUpperCase()+") <a href='/search.html?activeFields=%7B\"provider\"%3A%5B\""+escape(item)+"\"%5D%7D' class='box-link'>SEARCH "+item.toUpperCase()+" CONTENT</a></span>";
-                    
-                    // add the media
-                    mediaAmount['videos'] += countryProvider[item].videos;
-                    mediaAmount['audios'] += countryProvider[item].audios;
-                    mediaAmount['images'] += countryProvider[item].images;
-                    mediaAmount['texts'] += countryProvider[item].texts;
-                    mediaAmount['series'] += countryProvider[item].series;
-                }
-
-                // get general info like country name
-                $('#selected-country').html(mapData[code].country);
-
-                // set amount of medias
-                $('#selected-videos').html(mediaAmount['videos']);
-                $('#selected-audios').html(mediaAmount['audios']);
-                $('#selected-images').html(mediaAmount['images']);
-                $('#selected-texts').html(mediaAmount['texts']);
-                $('#selected-series').html(mediaAmount['series']);
-
-                // set providers info
-                //$('#selected-searchlink').html(providerLink);
-                $('#selected-providers').html(providerList);
-            } else {
-
-                // show alert when no provider
-                alert("No EUscreen provider from "+region);
-            }
-        }
-    });
-
-    // retrieve providers data (country, the amount of videos, audios etc.) from json file
-    // file has to be updated
-    $.getJSON( "/eddie/apps/euscreenxlelements/libs/jqvmap/data/euscreen.provider.data.json", function( data ) {
-        
-        // set variable
-        mapData = data;
-
-        // highlight countries in euscreen
-        // for easier search on the map
-        var colorsST = {},
-            highlightColor = "#c0c1c5";
-
-        // loop
-        for (var item in data) {
-            colorsST[item] = highlightColor;
-        }
-        
-        console.log(colorsST);
-        
-        // set
-        $('#vmap').vectorMap('set', 'colors', colorsST);
-    });
-};
--- src/org/springfield/lou/application/types/EuscreenxlmapApplication.java
+++ src/org/springfield/lou/application/types/EuscreenxlmapApplication.java
@@ -1,6 +1,7 @@
 package org.springfield.lou.application.types;
 
 import org.springfield.lou.application.Html5Application;
+import org.springfield.lou.screen.Screen;
 
 public class EuscreenxlmapApplication extends Html5Application{
 		
@@ -12,11 +13,27 @@ public class EuscreenxlmapApplication extends Html5Application{
 		this.addReferid("footer", "/euscreenxlelements/footer");
 		this.addReferid("terms", "/euscreenxlelements/terms");
 		this.addReferid("linkinterceptor", "/euscreenxlelements/linkinterceptor");
+		this.addReferid("favicon", "/euscreenxlelements/favicon");
+		this.addReferid("history", "/euscreenxlelements/history");
 		
 		this.addReferidCSS("elements", "/euscreenxlelements/generic");
 		this.addReferidCSS("bootstrap", "/euscreenxlelements/bootstrap");
 		this.addReferidCSS("jqvmap", "/euscreenxlelements/jqvmap");
-		
+				
 	}
-
+ 	
+ 	public void initMap(Screen s){
+ 		System.out.println("initMap()");
+ 		String regionCode = s.getParameter("code");
+ 		 		
+ 		String command;
+ 		if(regionCode != null){
+ 			command = "initMap(" + regionCode + ")";
+ 		}else{
+ 			command = "initMap()";
+ 		}
+ 		
+ 		s.putMsg("template", "", command);
+ 		
+ 	};
 }
